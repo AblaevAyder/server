9af017614707eb5d6350dcf796f29924d1c3812f
Ayduxa <ablaev.a.i.1.18@gmail.com>
2018-10-17 17:22:49 +0300

    added file

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-  res.send("привет от сервера")})
+  res.send("привет от Cервера")})
 
 app.listen("3000");
